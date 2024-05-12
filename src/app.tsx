12404602eef84dcf84bf0f06a486af226c320d50
Reginaldo Lima <108771153+eusouregislima@users.noreply.github.com>
2024-05-12 17:33:27 -0300

    start login screen

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,9 @@
-function App() {
+import { Login } from './screens/login';
+
+export function App() {
   return (
     <>
-      <h1>Hello Elite</h1>
+      <Login />
     </>
   );
 }
-
-export default App;
