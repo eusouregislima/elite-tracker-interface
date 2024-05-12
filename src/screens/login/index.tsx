7f146995da7307e32f74ca9e158513b5f97b7142
Reginaldo Lima <108771153+eusouregislima@users.noreply.github.com>
2024-05-12 18:01:16 -0300

    continuando a tela de login

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -3,7 +3,14 @@ import styles from './styles.module.css';
 export function Login() {
   return (
     <div className={styles.container}>
-      <h1>Login</h1>
+      <div className={styles.content}>
+        <h1>Entre com</h1>
+        <button>GitHub</button>
+        <p>
+          Ao entrar, eu concordo com os Termos de Serviço e Política de
+          Privacidade.
+        </p>
+      </div>
     </div>
   );
 }
