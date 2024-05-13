8352e9eeab9a73db962ba583131937539fd39149
Reginaldo Lima <108771153+eusouregislima@users.noreply.github.com>
2024-05-13 12:45:26 -0300

    screen Login and phosphor-icons

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,3 +1,5 @@
+import { GithubLogo } from '@phosphor-icons/react';
+
 import styles from './styles.module.css';
 
 export function Login() {
@@ -5,7 +7,10 @@ export function Login() {
     <div className={styles.container}>
       <div className={styles.content}>
         <h1>Entre com</h1>
-        <button>GitHub</button>
+        <button>
+          <GithubLogo />
+          GitHub
+        </button>
         <p>
           Ao entrar, eu concordo com os Termos de Serviço e Política de
           Privacidade.
